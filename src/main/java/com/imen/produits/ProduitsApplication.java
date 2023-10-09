986b3a3543c09b955023e3aa90edb9c0eba34a44
imene khacheche <imenekhacheche@gmail.com>
2023-10-09 22:16:06 +0100

    init

---
+++ src/main/java/com/imen/produits/ProduitsApplication.java
@@ -0,0 +1,17 @@
+package com.imen.produits;
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProduitsApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitsApplication.class, args);
+	}
+
+}
